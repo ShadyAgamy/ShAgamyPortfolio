18e96e0d8d69cc68a0b986c94466ab1c03a899f8
shadyagmy <72800568+ShadyAgamy@users.noreply.github.com>
2021-12-15 12:01:18 +0200

    first commit!

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Menu from "./components/Menu/Menu";
+import HomePage from "./pages/homePage/HomePage";
+import About from "./pages/about/About";
+import Resume from "./pages/resume/Resume";
+import Portfolio from "./pages/portfolio/Portfolio";
+import Contact from "./pages/contact/Contact";
+
+import "./reset.scss"
+import './App.scss';
+
+
+class App extends React.Component {
+
+  state= {
+    menuOpen: true
+  }
+
+  componentDidMount =() => {
+      window.addEventListener("resize", this.resize);
+      this.resize();
+  }
+
+  resize =() =>  {
+      this.setState({menuOpen: window.innerWidth >= 900});
+  }
+
+  componentWillUnmount =() =>  {
+      window.removeEventListener("resize", this.resize);
+  }
+  
+  toggleMenu = () => {
+    if (window.innerWidth <= 900) {
+      this.setState({menuOpen: !this.state.menuOpen})
+    }
+      
+  } 
+
+
+  render() {
+    return (
+      <div className="page_container">
+        <Menu  menuOpen={this.state.menuOpen} toggleMenu={() =>this.toggleMenu()}/>
+        <div className="width_80" >
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route exact path='/about' component={About} />
+            <Route exact path='/resume' component={Resume} />
+            <Route exact path='/portfolio' component={Portfolio} />
+            <Route exact path='/contact' component={Contact} />
+          </Switch>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+export default App ;
---
+++ src/pages/homePage/HomePage.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+
+import Particles from 'react-particles-js';
+
+import "./homepage.scss";
+
+const practicles_config = {
+    style: {
+        height : "100%",
+        background: "#000",
+    },
+    params : {
+	    "particles": {
+	        "number": {
+	            "value": 160,
+	            "density": {
+	                "enable": false
+	            }
+	        },
+	        "size": {
+	            "value": 6,
+	            "random": true,
+	            "anim": {
+	                "speed": 4,
+	                "size_min": 0.7
+	            }
+	        },
+	        "line_linked": {
+	            "enable": false
+	        },
+	        "move": {
+	            "random": true,
+	            "speed": 1,
+	            "direction": "top",
+	            "out_mode": "out"
+	        },
+            "opacity": {
+                "value": .1,
+                "random": false, // Set to false in our case
+                
+              }
+	    }
+       
+    }
+}
+
+
+export default function HomePage() {
+    return (
+        <div className="homepage" style={practicles_config.style}>
+            <Particles params={practicles_config.params}/>
+            <div className="homepage_section">
+                <h1>Hi, I am <span className="co_main">Shady Agamy</span></h1>
+                <p>I am a frontend web developer. I can provide clean code and pixel perfect design. I also make website more &#38; more interactive with web animations.</p>
+                <div className="links">
+                    <a href="https://www.linkedin.com/in/shady-agamy-746a59147/" target="_blank"  rel="noopener noreferrer"><i className="fab fa-linkedin-in"></i></a>
+                    <a href="https://github.com/shadyagmy" target="_blank"  rel="noopener noreferrer"><i className="fab fa-github"></i></a>
+                    <a href="https://codepen.io/shady-agmy" target="_blank"  rel="noopener noreferrer"><i className="fab fa-codepen"></i></a>
+                </div>
+            </div>
+        </div>
+    )
+}
