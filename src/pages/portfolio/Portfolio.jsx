ace2bd1b0ee7610cbb6d7bfc479052211c97a281
shadyagmy <72800568+ShadyAgamy@users.noreply.github.com>
2021-12-16 07:40:59 +0200

    change porfolio section into categorized tabs

--- src/pages/portfolio/Portfolio.jsx
+++ src/pages/portfolio/Portfolio.jsx
@@ -1,62 +1,106 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 import "./portfolio.scss";
 
-import ReactPaginate from 'react-paginate';
+import ReactPaginate from "react-paginate";
+
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
 import portfolioData from "./portfolio.data";
 
 export default function Portfolio() {
+  //   const [currentPage, setCurrentPage] = useState(0);
+  const [data] = useState(portfolioData);
 
-    const [currentPage, setCurrentPage] = useState(0);
-    const [data] = useState(portfolioData);
+  //   const PER_PAGE = 6;
+  //   const offset = currentPage * PER_PAGE;
+  const reactProjects = data.filter(({ type }) => {
+    return type === "react";
+  });
 
+  const jqueryProjects = data.filter(({ type }) => {
+    return type === "jquery";
+  });
 
-    const PER_PAGE = 6;
-    const offset = currentPage * PER_PAGE;
-    const currentPageData = data
-        .slice(offset, offset + PER_PAGE)
-            .map(({id, name, url, desc, imgSrc }) => {
-                return (
-                    <div key={id} className="port_item">
-                        <div className="back_img" style={{backgroundImage: `url(${imgSrc})`}}>
-                            <div className="link_div"></div>
-                            <a className="app_link" href={url} target="_blank"  rel="noopener noreferrer"><i className="fas fa-link"></i></a>
-                        </div>
-                        <h3>{name}</h3>
-                        <p> {desc} </p>
-                    </div>
-                )
-            }
-        );
-    const pageCount = Math.ceil(data.length / PER_PAGE);
+  const htmlProjects = data.filter(({ type }) => {
+    return type === "html";
+  });
 
-    function handlePageClick({ selected: selectedPage }) {
-        setCurrentPage(selectedPage);
-    }
-
-    const chevLeft = <i className="fas fa-chevron-left"></i>;
-    const chevRight = <i className="fas fa-chevron-right"></i>;
-    return (
-        <div className="about_page portfolio_page">
-            <h2 className="main_heading">PORTFOLIO <span></span><p className="shadow">PORTFOLIO</p></h2>
-            <div className="port_items">
-                {currentPageData}
-            </div>
-            
-            <ReactPaginate
-                previousLabel={chevLeft}
-                nextLabel={chevRight}
-                pageCount={pageCount}
-                onPageChange={handlePageClick}
-                containerClassName={"pagination"}
-                previousLinkClassName={"pagination__link"}
-                nextLinkClassName={"pagination__link"}
-                disabledClassName={"pagination__link--disabled"}
-                activeClassName={"pagination__link--active"}
-            />
-            
+  //   const currentPageData = data
+  // .slice(offset, offset + PER_PAGE)
+  const renderProjects = (projects) => {
+    return projects.map(({ id, name, url, desc, imgSrc }) => {
+      return (
+        <div key={id} className="port_item">
+          <div
+            className="back_img"
+            style={{ backgroundImage: `url(${imgSrc})` }}
+          >
+            <div className="link_div"></div>
+            <a
+              className="app_link"
+              href={url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fas fa-link"></i>
+            </a>
+          </div>
+          <h3>{name}</h3>
+          <p> {desc} </p>
         </div>
-    )
-}
+      );
+    });
+  };
 
+  //   const pageCount = Math.ceil(data.length / PER_PAGE);
+
+  //   function handlePageClick({ selected: selectedPage }) {
+  //     setCurrentPage(selectedPage);
+  //   }
+
+  //   const chevLeft = <i className="fas fa-chevron-left"></i>;
+  //   const chevRight = <i className="fas fa-chevron-right"></i>;
+  return (
+    <div className="about_page portfolio_page">
+      <h2 className="main_heading">
+        PORTFOLIO <span></span>
+        <p className="shadow">PORTFOLIO</p>
+      </h2>
+
+      <Tabs>
+        <TabList>
+          <Tab>
+              React Projects
+            {/* <i className="fab fa-react"></i> */}
+          </Tab>
+          <Tab>
+              Jquery Projects
+            {/* <i className="fab fa-js"></i> */}
+          </Tab>
+          <Tab>
+              UI demo Projects
+            {/* <i className="fab fa-html5"></i> */}
+          </Tab>
+        </TabList>
+
+        <TabPanel> <div className="port_items"> {reactProjects && renderProjects(reactProjects)}</div></TabPanel>
+        <TabPanel> <div className="port_items"> {jqueryProjects && renderProjects(jqueryProjects)}</div></TabPanel>
+        <TabPanel> <div className="port_items"> {htmlProjects && renderProjects(htmlProjects)}</div></TabPanel>
+      </Tabs>
+
+      {/* <ReactPaginate
+        previousLabel={chevLeft}
+        nextLabel={chevRight}
+        pageCount={pageCount}
+        onPageChange={handlePageClick}
+        containerClassName={"pagination"}
+        previousLinkClassName={"pagination__link"}
+        nextLinkClassName={"pagination__link"}
+        disabledClassName={"pagination__link--disabled"}
+        activeClassName={"pagination__link--active"}
+      /> */}
+    </div>
+  );
+}
