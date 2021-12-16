e47776b7937810193567f18eddbbb90cd8ed7e19
shadyagmy <72800568+ShadyAgamy@users.noreply.github.com>
2021-12-16 07:40:59 +0200

    change porfolio section into categorized tabs

--- src/pages/portfolio/portfolio.data.js
+++ src/pages/portfolio/portfolio.data.js
@@ -1,16 +1,16 @@
 const  portfolioData = [
-    {id: "CRWN-app", name: "CRWN-app", url:"https://crwn-ecomm-demo-app.herokuapp.com/", desc:"React, Redux E-Commerce demo app", imgSrc : "./imgs/projects/crwn.png"},
-    {id: "Shotmed", name: "Shotmed", url:"https://shadyagmy.github.io/Shotmed/build/", desc:"HTML5, CSS3, JQUERY Modern website", imgSrc : "./imgs/projects/shotmed.png"},
-    {id: "EFS", name: "EFS", url:"https://www.efsmi.com/en/", desc:"HTML5, CSS3, JQUERY Modern website", imgSrc : "./imgs/projects/EFS.png"},
-    {id: "CastHub", name: "CastHub", url:"https://www.cast-hub.tv/en", desc:"HTML5, CSS3, JQUERY web application", imgSrc : "./imgs/projects/castHub.png"},
-    {id: "RAYA CC", name: "RAYA CC", url:"https://rayacc.com/", desc:"HTML5, CSS3, JQUERY Modern website", imgSrc : "./imgs/projects/rayacc.png"},
-    {id: "NATOUR", name: "NATOUR", url:"https://shadyagmy.github.io/Natour/", desc:"HTML5, CSS3 modern landing page", imgSrc : "./imgs/projects/natour.png"},
-    {id: "EKUITY", name: "EKUITY", url:"https://shadyagmy.github.io/Ekuity-LP/", desc:"HTML5, CSS3, JQUERY modern demo landing page", imgSrc : "./imgs/projects/Ekuity.png"},
-    {id: "Nexter", name: "Nexter", url:"https://shadyagmy.github.io/Nexter/", desc:"Modern landing page have totally created using the CSS3 Grid property", imgSrc : "./imgs/projects/Nexter.png"},
+    {id: "CRWN-app", name: "CRWN-app", url:"https://crwn-ecomm-demo-app.herokuapp.com/", desc:"React, Redux E-Commerce demo app", imgSrc : "./imgs/projects/crwn.png", type: "react"},
+    {id: "Shotmed", name: "Shotmed", url:"https://shadyagmy.github.io/Shotmed/build/", desc:"HTML5, CSS3, JQUERY Modern website", imgSrc : "./imgs/projects/shotmed.png" , type: "jquery"},
+    {id: "EFS", name: "EFS", url:"https://www.efsmi.com/en/", desc:"HTML5, CSS3, JQUERY Modern website", imgSrc : "./imgs/projects/EFS.png", type: "jquery"},
+    {id: "CastHub", name: "CastHub", url:"https://www.cast-hub.tv/en", desc:"HTML5, CSS3, JQUERY web application", imgSrc : "./imgs/projects/castHub.png", type: "jquery"},
+    {id: "RAYA CC", name: "RAYA CC", url:"https://rayacc.com/", desc:"HTML5, CSS3, JQUERY Modern website", imgSrc : "./imgs/projects/rayacc.png", type: "jquery"},
+    {id: "NATOUR", name: "NATOUR", url:"https://shadyagmy.github.io/Natour/", desc:"HTML5, CSS3 modern landing page", imgSrc : "./imgs/projects/natour.png", type: "jquery"},
+    {id: "EKUITY", name: "EKUITY", url:"https://shadyagmy.github.io/Ekuity-LP/", desc:"HTML5, CSS3, JQUERY modern demo landing page", imgSrc : "./imgs/projects/Ekuity.png", type: "jquery"},
+    {id: "Nexter", name: "Nexter", url:"https://shadyagmy.github.io/Nexter/", desc:"Modern landing page have totally created using the CSS3 Grid property", imgSrc : "./imgs/projects/Nexter.png", type: "html"},
     {id: "YOUR OWN WEBSITE", name: "YOUR OWN WEBSITE", url:"https://shadyagmy.github.io/Your-website/", desc:"Modern landing page have created using Html,CSS3 and ES5.", imgSrc : "./imgs/projects/your_website.png"},
-    {id: "TRILLO", name: "TRILLO", url:"https://shadyagmy.github.io/Trillo-/", desc:"Modern web application landing page was totally structured with the CSS3 flex box property", imgSrc : "./imgs/projects/trillo.png"},
-    {id: "Conrad", name: "Conrad", url:"https://shadyagmy.github.io/Conrad/", desc:"Simple landing page have created using Html, Css and Javascript", imgSrc : "./imgs/projects/conrad.png"},
-    {id: "TINKER", name: "TINKER", url:"https://shadyagmy.github.io/Tinker-web-site-page/", desc:"Simple landing page was created using Html, Css and ES5", imgSrc : "./imgs/projects/tinker.png"},
+    {id: "TRILLO", name: "TRILLO", url:"https://shadyagmy.github.io/Trillo-/", desc:"Modern web application landing page was totally structured with the CSS3 flex box property", imgSrc : "./imgs/projects/trillo.png", type: "html"},
+    {id: "Conrad", name: "Conrad", url:"https://shadyagmy.github.io/Conrad/", desc:"Simple landing page have created using Html, Css and Javascript", imgSrc : "./imgs/projects/conrad.png", type: "html"},
+    {id: "TINKER", name: "TINKER", url:"https://shadyagmy.github.io/Tinker-web-site-page/", desc:"Simple landing page was created using Html, Css and ES5", imgSrc : "./imgs/projects/tinker.png", type: "html"},
 ];
 
 export default portfolioData
