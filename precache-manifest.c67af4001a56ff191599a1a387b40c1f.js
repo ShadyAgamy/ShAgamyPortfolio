7d73b8b618fe217b58c8d473d01c8d6525953054
shadyagmy <72800568+ShadyAgamy@users.noreply.github.com>
2022-01-08 17:06:00 +0200

    Updates

---
+++ precache-manifest.c67af4001a56ff191599a1a387b40c1f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea998fda36052849ddc600e9bcf9315e",
+    "url": "/ShAgamyPortfolio/index.html"
+  },
+  {
+    "revision": "a4de28a8a16d4c1f5fb9",
+    "url": "/ShAgamyPortfolio/static/css/2.efc52865.chunk.css"
+  },
+  {
+    "revision": "ea116c7c832c4f7f3a2d",
+    "url": "/ShAgamyPortfolio/static/css/main.f948b858.chunk.css"
+  },
+  {
+    "revision": "a4de28a8a16d4c1f5fb9",
+    "url": "/ShAgamyPortfolio/static/js/2.c3b824e7.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/ShAgamyPortfolio/static/js/2.c3b824e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea116c7c832c4f7f3a2d",
+    "url": "/ShAgamyPortfolio/static/js/main.4cc59254.chunk.js"
+  },
+  {
+    "revision": "3cca0d8e1007df819b0d",
+    "url": "/ShAgamyPortfolio/static/js/runtime-main.ab944a60.js"
+  },
+  {
+    "revision": "41473127edeae2504a979d98d1b84122",
+    "url": "/ShAgamyPortfolio/static/media/avatar.41473127.svg"
+  },
+  {
+    "revision": "f6c2d61ca803d0ce2fa49c3db4998987",
+    "url": "/ShAgamyPortfolio/static/media/info.f6c2d61c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6ced96ebe7a401db0019623a4b187ec6.js"
+  "/ShAgamyPortfolio/precache-manifest.c67af4001a56ff191599a1a387b40c1f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ShAgamyPortfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
